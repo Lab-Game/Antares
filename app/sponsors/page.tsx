e01acf67cf3b509f0da34760fe8e55b265085a18
Lab-Game <eric.lehman@gmail.com>
2026-09-23 10:31:17 -0700

    add more sponsors

--- app/sponsors/page.tsx
+++ app/sponsors/page.tsx
@@ -28,6 +28,69 @@ export default function SponsorsPage() {
         width={550}
         height={119}
       />
+      <Image
+        className={styles.logo}
+        src="/sponsor-logos/Abbott_Laboratories-logo.webp"
+        alt="Abbott Laboratories Logo"
+        width={1000}
+        height={250}
+      />
+      <Image
+        className={styles.logo}
+        src="/sponsor-logos/Altair_Engineering-logo.svg"
+        alt="Altair Engineering Logo"
+        width={263}
+        height={54}
+      />
+      <Image
+        className={styles.logo}
+        src="/sponsor-logos/Apple-logo.svg"
+        alt="Apple Logo"
+        width={44}
+        height={54}
+      />
+      <Image
+        className={styles.logo}
+        src="/sponsor-logos/General_Catalyst-logo.svg"
+        alt="General Catalyst Logo"
+        width={400}
+        height={54}
+      />
+      <Image
+        className={styles.logo}
+        src="/sponsor-logos/GlobalLogic-logo.svg"
+        alt="GlobalLogic Logo"
+        width={800}
+        height={182}
+      />
+      <Image
+        className={styles.logo}
+        src="/sponsor-logos/haas-logo.svg"
+        alt="Haas Logo"
+        width={181}
+        height={72}
+      />
+      <Image
+        className={styles.logo}
+        src="/sponsor-logos/HalloApp-logo.svg"
+        alt="HalloApp Logo"
+        width={259}
+        height={54}
+      />
+      <Image
+        className={styles.logo}
+        src="/sponsor-logos/Legion_Technologies-logo.svg"
+        alt="Legion Technologies Logo"
+        width={800}
+        height={143}
+      />
+      <Image
+        className={styles.logo}
+        src="/sponsor-logos/Saints_Capital-logo.svg"
+        alt="Saints Capital Logo"
+        width={170}
+        height={42}
+      />
     </main>
   );
 }
